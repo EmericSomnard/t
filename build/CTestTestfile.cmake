be5e7ad7efed87a30a4baa09e1eab85ae28eded0
ESomnard <emeric.somnard@epitech.eu>
2024-11-27 08:54:34 +0100

    t

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/e/Documents/tek3/github/t
-# Build directory: /home/e/Documents/tek3/github/t/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(ServerTest "/home/e/Documents/tek3/github/t/build/server_test")
-set_tests_properties(ServerTest PROPERTIES  _BACKTRACE_TRIPLES "/home/e/Documents/tek3/github/t/CMakeLists.txt;19;add_test;/home/e/Documents/tek3/github/t/CMakeLists.txt;0;")
